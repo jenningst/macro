37f103c253281b0e1237c34f3ad12a6da75885a0
Troy Jennings <troy.c.jennings@gmail.com>
2019-03-22 11:24:00 +0100

    Refactored mealResolver
    - Cleaned up updateMeal logic (try/catches and error handling)
    
    - Resolves:
      - https://github.com/jenningst/macro/issues/2
      - https://github.com/jenningst/macro/issues/3
      - https://github.com/jenningst/macro/issues/4

--- server/src/graphql/resolvers/mealResolver.js
+++ server/src/graphql/resolvers/mealResolver.js
@@ -24,7 +24,6 @@ module.exports = {
   Mutation: {
     createMeal: async function(parent, { input }) {
       const { name, position, owner } = input;
-
       // prepare our response payload
       let response = {
         food: null,
@@ -35,50 +34,69 @@ module.exports = {
         const meal = await Meal.findOne({ name });
         console.log(meal);
         if (meal) {
-          response.error = { message: "Please provide a unique food name!" };
+          response.error = {
+            message: "Please provide a unique food name!"
+          };
           return response;
-        } else {
-          // mongoose: create a new instance of Meal
-          const newMeal = new Meal({
-            name: name,
-            position: position || null,
-            owner: "5c93b4965529ad0d65e4b103" // TODO: remove hard-coding later
-          });
-
-          // mongoose: save the meal and format the response
-          try {
-            const updatedMeal = await newMeal.save();
-            response.meal = updatedMeal;
-            response.error = {};
-          } catch (error) {
-            response.error = {
-              message: `Failed to create food with error: ${error}`
-            };
-          }
+        }
+        // mongoose: create a new instance of Meal
+        const newMeal = new Meal({
+          name: name,
+          position: position || null,
+          owner: "5c93b4965529ad0d65e4b103" // TODO: remove hard-coding later
+        });
+        // mongoose: save the meal and format the response
+        try {
+          const updatedMeal = await newMeal.save();
+          response.meal = updatedMeal;
+          response.error = {};
+        } catch (error) {
+          response.error = {
+            message: `Failed to create food with error: ${error}`
+          };
         }
       } catch (error) {
-        response.error = { message: `Error finding meal: ${error}` };
+        response.error = {
+          message: `Error finding meal: ${error}`
+        };
       }
-
       // return
       return response;
     },
-    updateMeal: async function(parent, { input: { id, name, position } }) {
+    updateMeal: async function(parent, { input }) {
+      const { id, name, position } = input;
+      // prepare our response payload
+      let response = {
+        food: null,
+        error: {}
+      };
       // mongoose: check for existing meal
-      let existingMeal = await Meal.findById(id);
-      if (!existingMeal) {
-        throw new Error(`Failed to find meal with id: ${id}!`);
-      }
-      // update existing meal
-      existingMeal.name = name || existingMeal.name;
-      existingMeal.position = position || existingMeal.position;
-      // mongoose: save
       try {
-        return await existingMeal.save();
+        const existingMeal = await Meal.findById(id);
+        if (!existingMeal) {
+          response.error = {
+            message: `Error finding meal: ${error}`
+          };
+          return response;
+        }
+        // update existing meal
+        existingMeal.name = name || existingMeal.name;
+        existingMeal.position = position || existingMeal.position;
+        // mongoose: save
+        try {
+          let updatedMeal = await existingMeal.save();
+          response.meal = updatedMeal;
+          response.error = {};
+        } catch (error) {
+          response.error = {
+            message: `Failed to update meal: ${error}`
+          };
+        }
       } catch (error) {
-        // TODO: Add custom error handling
-        throw new Error(`Failed to create meal with error: ${error}`);
+        response.error = { message: `Error during findById: ${error}` };
       }
+      // return
+      return response;
     }
   }
 };
